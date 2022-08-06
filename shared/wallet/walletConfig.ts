a1faec0b13444a580dacefc25f1e363bb6160aa5
mounikahalde <hmounika1997@gmail.com>
2022-08-06 12:38:01 +0530

    fixed build issuse

--- shared/wallet/walletConfig.ts
+++ shared/wallet/walletConfig.ts
@@ -1,11 +1,11 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit"
 import { chain, configureChains, createClient } from "wagmi"
-import { alchemyProvider } from "wagmi/providers/alchemy"
+// import { alchemyProvider } from "wagmi/providers/alchemy"
 import { publicProvider } from "wagmi/providers/public"
 
 export const { chains, provider } = configureChains(
   [chain.ropsten],
-  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+  [publicProvider()]
 )
 
 const { connectors } = getDefaultWallets({
